3a00419558752d216a406edabfb4b3da92f902b9
enestepekoy@gmail.com <enestepekoy@gmail.com>
2023-04-27 00:33:00 +0300

    initial commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import logo from './logo.svg';
+import './App.css';
+import './styles.css';
+
+
+function App() {
+  return(
+    <main>
+      Hello World.
+    </main>
+  );
+   
+      
+}
+export default App;
