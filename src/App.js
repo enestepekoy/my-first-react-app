311893cccb8236051bad22b275dea7ee04ece2d5
enestepekoy@gmail.com <enestepekoy@gmail.com>
2023-04-28 03:33:59 +0300

    d

--- src/App.js
+++ src/App.js
@@ -1,13 +1,25 @@
 import logo from './logo.svg';
 import './App.css';
 import './styles.css';
+import Footer from'./Footer';
+import Header from './Header';
+import Employees from './Employees';
+import Counter from './Counter';
+
 
 
 function App() {
+  // const [employees, setEmployees] = useState(JSON.parse(localStorage.getItem('employeeList')) [I Hi
+    
+
   return(
-    <main>
-      Hello World.
-    </main>
+    <div>
+      <Header/>
+      <Employees/>
+      <Footer/>
+      <Counter/>
+    </div>
+    
   );
    
       
--- src/Header.js
+++ src/Header.js
@@ -1,1 +1,10 @@
-const Header = 
+const header = () => {
+    return (
+        <header>
+            <h1 className="mt-4 p5 bg-primary text-white rounded">Header</h1>
+        </header>
+        
+    )
+}
+
+export default header
